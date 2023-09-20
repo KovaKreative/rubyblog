b8fed424d9b3dd3704dab22c62a0dd78f46642df
KovaKreative <eric.kovalevskyy@gmail.com>
2023-09-20 15:12:22 -0400

    Create post controller/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  resources :posts
+
+  # get '/posts', to: 'posts#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
