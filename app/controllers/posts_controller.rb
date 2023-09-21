2df90f483887b8468e7b50e05c46aaa910127500
KovaKreative <eric.kovalevskyy@gmail.com>
2023-09-21 14:49:09 -0400

    Implement creating new posts and notices

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,22 +2,34 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
   def new
     @post = Post.new
     @category = Category.all
   end
+
+  def create
+    params.permit!
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path, :notice => "Your entry has been posted!"
+    else
+      render "new"
+    end
+  end
+
   def edit
   
   end
-  def create
-    
-  end
+
   def update
   
   end
+
   def destroy
   
   end
