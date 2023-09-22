d577291e854a9a481bb82b88233f45beb620fb0c
KovaKreative <eric.kovalevskyy@gmail.com>
2023-09-22 14:40:01 -0400

    Implement deleting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,19 +25,22 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
-
+  
   def update
     params.permit!
     @post = Post.find(params[:id])
-
+    
     if @post.update(params[:post])
       redirect_to post_path, :notice => "Your post has been updated"
     else
       render "edit"
     end
   end
-
-  def destroy
   
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path, :notice => "Your post has been deleted"
   end
 end
