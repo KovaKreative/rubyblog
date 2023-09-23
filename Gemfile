f2fcadf5c880bb813e5d4b8fd5d1d85935a27da8
KovaKreative <eric.kovalevskyy@gmail.com>
2023-09-22 21:22:07 -0400

    Implement admin dashboard

--- Gemfile
+++ Gemfile
@@ -14,6 +14,11 @@ gem "sprockets-rails"
 # Use postgres as the database for Active Record
 gem "pg"
 
+gem "activeadmin"
+gem "devise"
+
+gem 'sass-rails'
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # get 'categories/index'
   # get 'categories/edit'
   # get 'categories/new'
   # get 'categories/show'
-
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  # root to: 'home#index'
 
+  get '/', to: 'home#index'
+  
   resources :posts
   resources :categories
-
+  
   # get '/posts', to: 'posts#index'
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
